e6f323329e5cac5ac90537d8916308b2915a6bae
ThyeeZz <jialian.ji@zilliz.com>
2021-12-24 15:58:22 +0800

    stash

---
+++ src/components/layout/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LIST = [
+  {
+    label: 'Pipelines',
+    href: '',
+  },
+  {
+    label: 'Operators',
+    href: '',
+  },
+  {
+    label: 'Documentation',
+    href: '',
+  },
+  {
+    label: 'Demo',
+    href: '',
+  },
+];
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,16 +1,31 @@
+import { makeStyles, Theme } from '@material-ui/core';
+import { Link } from '@material-ui/core';
+import Image from 'next/image';
+import { NAV_LIST } from './constants';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  headerWrapper: {},
+}));
 
-
-
-const Header:React.FC<any> = ()=>{
-
+const Header: React.FC<any> = () => {
+  const classes = useStyles();
   return (
-    <section style={{height: '200px',border: '1px solid blue'}}>
-      
-      Header
-    </section>
-  )
+    <header style={{ height: '200px', border: '1px solid blue' }}>
+      <div className="left-part">
+        <Link href="/">
+          <Image src={'/images/logo-title.png'}></Image>
+        </Link>
+      </div>
+      <div className="right-part">
+        <div className="git-btn"></div>
+        <ul className="nav-list">
+          {NAV_LIST.map(v => (
+            <Link href={v.href}>{v.label}</Link>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-}
-
-export default Header
+export default Header;
